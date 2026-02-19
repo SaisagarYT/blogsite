e36154292352198aa7cfaefef5f8001da5689026
SaisagarYT <saisagarsylada@gmail.com>
2026-02-19 16:14:35 +0530

    updated cards

--- src/reusableComponents/HomepageFeatureCard.jsx
+++ src/reusableComponents/HomepageFeatureCard.jsx
@@ -4,7 +4,7 @@ import HomepageFeatureSubCard from './HomepageFeatureSubCard'
 
 const HomepageFeatureCard = ({title,para,icon}) => {
   return (
-    <section className='w-full flex px-30 border-b border-r border-l text-(--text-main) border-(--bg-primary)'>
+    <section className='w-full flex px-30 max-sm:flex-col max-sm:items-center max-sm:justify-center max-sm:w-full max-sm:px-2 border-b border-r border-l text-(--text-main) border-(--bg-primary)'>
       <div className='flex flex-1/5 py-30 flex-col items-start border-r mr-10 border-(--bg-primary)  justify-center'>
         <img className='scale-75' src={icon} alt="" />
         <br />
@@ -12,7 +12,7 @@ const HomepageFeatureCard = ({title,para,icon}) => {
         <br />
         <p className='text-(--text-secondary)'>{para}</p>
       </div>
-      <div className='grid grid-cols-2 py-10 gap-10 flex-1/2'>
+      <div className='grid grid-cols-2 py-10 max-sm:p-0 max-sm:grid-cols-1 gap-10 flex-1/2'>
         <HomepageFeatureSubCard title="Quality" text="Over 1,000 articles on emerging tech trends and breakthroughs."/>
         <HomepageFeatureSubCard title="Quality" text="Over 1,000 articles on emerging tech trends and breakthroughs."/>
         <HomepageFeatureSubCard title="Quality" text="Over 1,000 articles on emerging tech trends and breakthroughs."/>
