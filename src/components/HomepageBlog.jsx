13b5d6a26c03988d9c4ac1a80904a00e22d4607c
SaiSagar <saisagarsylada@gmail.com>
2026-03-03 16:22:59 +0530

    fixed the navbar issue

--- src/components/HomepageBlog.jsx
+++ src/components/HomepageBlog.jsx
@@ -4,23 +4,20 @@ import Webassets from '../assets/Assets'
 
 const HomepageBlog = ({image,name,tag,date,title,description,likes,comments,shares}) => {
   return (
-    <section className='w-full px-2 md:px-4 lg:px-30 max-sm:flex-col max-sm:items-start border flex flex-col md:flex-row md:items-center border-(--bg-primary)'>
-        <div className='flex-1/8 py-8 md:py-20 flex gap-3 md:gap-5 items-center flex-shrink-0'>
+    <section className='w-full border border-(--bg-primary) p-4 md:p-5 lg:p-6 flex flex-col items-start gap-3 md:gap-4 h-full'>
+        <div className='w-full flex gap-3 items-center'>
           <img src={image} alt={name} className='w-10 md:w-12 h-10 md:h-12 rounded-full object-cover' />
           <div className=''>
             <p className='text-xs md:text-sm font-semibold'>{name}</p>
             <p className='text-(--text-secondary) text-xs md:text-sm'>{tag}</p>
           </div>
         </div>
-        <div className='flex-1/2 flex flex-col gap-1 md:gap-2 px-2 md:px-4'>
+        <div className='w-full flex flex-col gap-1 md:gap-2'>
           <p className='text-xs md:text-sm text-(--text-secondary)'>{date}</p>
           <h1 className='text-lg md:text-xl lg:text-2xl font-semibold line-clamp-2'>{title}</h1>
-          <p className='text-(--text-secondary) text-xs md:text-sm line-clamp-2'>{description}</p>
-            <div>
-
-            </div>
+          <p className='text-(--text-secondary) text-xs md:text-sm line-clamp-3'>{description}</p>
         </div>
-        <div className='flex-shrink-0 mt-3 md:mt-0'>
+        <div className='mt-2'>
           <HomepageButton text="View Blog" icon={Webassets.arrowMark}/>
         </div>
       </section>
